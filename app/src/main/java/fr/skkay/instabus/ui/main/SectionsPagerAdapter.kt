b68c8e8c045c06def567515ab3eae567574686ac
Skkay <skkay.thomas@gmail.com>
2021-01-07 15:16:51 +0100

    Adding List fragment

--- app/src/main/java/fr/skkay/instabus/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/fr/skkay/instabus/ui/main/SectionsPagerAdapter.kt
@@ -5,11 +5,13 @@ import androidx.annotation.FloatRange
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import fr.skkay.instabus.ListFragment
 import fr.skkay.instabus.MapsFragment
 import fr.skkay.instabus.R
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1
+        R.string.tab_text_1,
+        R.string.tab_text_2
 )
 
 /**
@@ -20,10 +22,13 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-
-        var fragment: Fragment = MapsFragment()
-        when (position) {
-            0 -> fragment = MapsFragment()
+        var fragment: Fragment
+        fragment = when (position) {
+            0 -> ListFragment()
+            1 -> MapsFragment()
+            else -> {
+                ListFragment()
+            }
         }
         return fragment
     }
@@ -34,6 +39,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 1
+        return 2
     }
 }
