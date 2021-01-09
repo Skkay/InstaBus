08f81568f449b16e11d45daeb96aeee8031042f3
Skkay <skkay.thomas@gmail.com>
2021-01-09 15:53:32 +0100

    Parsing and displaying bus stations

--- app/src/main/java/fr/skkay/instabus/ui/main/fragments/ListFragment.kt
+++ app/src/main/java/fr/skkay/instabus/ui/main/fragments/ListFragment.kt
@@ -2,20 +2,21 @@ package fr.skkay.instabus.ui.main.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.skkay.instabus.R
 import fr.skkay.instabus.adapters.StationAdapter
 import fr.skkay.instabus.dataclass.StationItem
 import fr.skkay.instabus.services.StationService
 import kotlinx.android.synthetic.main.fragment_list.*
+import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.lang.Exception
-import java.lang.reflect.Executable
+
 
 class ListFragment : Fragment() {
 
@@ -41,10 +42,11 @@ class ListFragment : Fragment() {
         else {
             Log.i("json_res", "json response empty")
         }
+        val stationList = parseJSONBusStation(res)
 
         recycler_view.apply {
             layoutManager = LinearLayoutManager(activity)
-            adapter = StationAdapter(list)
+            adapter = StationAdapter(stationList)
         }
     }
 
@@ -86,6 +88,21 @@ class ListFragment : Fragment() {
             }
         })
         thread.start()
+        thread.join()
         return test_response
+    }
+
+    private fun parseJSONBusStation(json: String): List<StationItem> {
+        val root: JSONObject = JSONObject(json)
+        val data: JSONObject = root.getJSONObject("data")
+        val stations: JSONArray = data.getJSONArray("tmbs")
+
+        val list = ArrayList<StationItem>()
+        for (i in 0 until stations.length()) {
+            val stationObject: JSONObject = stations.getJSONObject(i)
+            val stationItem = StationItem(stationObject.getString("street_name"))
+            list += stationItem
+        }
+        return list
     }
 }
