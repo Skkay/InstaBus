1f5732a130b6576713663987d0a69c4ae86a8433
Skkay <skkay.thomas@gmail.com>
2021-01-15 16:53:11 +0100

    Database connection and adding item to it

--- app/src/main/java/fr/skkay/instabus/PhotoListActivity.kt
+++ app/src/main/java/fr/skkay/instabus/PhotoListActivity.kt
@@ -1,10 +1,14 @@
 package fr.skkay.instabus
 
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.skkay.instabus.adapters.PhotoAdapter
+import fr.skkay.instabus.contracts.PhotoContract
+import fr.skkay.instabus.databaseHelper.PhotoDBHelper
 import fr.skkay.instabus.dataclass.PhotoItem
 import kotlinx.android.synthetic.main.activity_photo_list.*
 import java.util.*
@@ -12,9 +16,15 @@ import kotlin.collections.ArrayList
 
 
 class PhotoListActivity : AppCompatActivity() {
+    lateinit var database: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_list)
+
+        // Database
+        val dbHelper = PhotoDBHelper(this)
+        database = dbHelper.writableDatabase
 
         val intent = intent
         Log.i("intent_result", "id : ${intent.getStringExtra("id")} ; streetName : ${intent.getStringExtra("streetName")}")
@@ -26,6 +36,8 @@ class PhotoListActivity : AppCompatActivity() {
             adapter = PhotoAdapter(dummyList)
             setHasFixedSize(true)
         }
+
+        addPhotoToDatabase()
     }
 
     private fun generateDummyList(size: Int): List<PhotoItem> {
@@ -37,5 +49,17 @@ class PhotoListActivity : AppCompatActivity() {
         }
 
         return list
+    }
+
+    private fun addPhotoToDatabase()
+    {
+        val item = PhotoItem("title", 0, Calendar.getInstance().getTime(), "id") // will be camera result
+
+        val cv = ContentValues()
+        cv.put(PhotoContract.PhotoEntry.COLUMN_TITLE, item.title)
+        cv.put(PhotoContract.PhotoEntry.COLUMN_IMAGE, item.image)
+        cv.put(PhotoContract.PhotoEntry.COLUMN_STATION_ID, item.station_id)
+
+        database.insert(PhotoContract.PhotoEntry.TABLE_NAME, null, cv)
     }
 }
