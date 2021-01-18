368a276002d2ae6e1311bb46f213172224730413
Skkay <skkay.thomas@gmail.com>
2021-01-18 13:52:00 +0100

    Removing FloatingActionButton in MainActivity

--- app/src/main/java/fr/skkay/instabus/MainActivity.kt
+++ app/src/main/java/fr/skkay/instabus/MainActivity.kt
@@ -1,8 +1,6 @@
 package fr.skkay.instabus
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
@@ -17,11 +15,5 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 }
