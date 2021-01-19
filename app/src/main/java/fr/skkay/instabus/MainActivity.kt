c4c950cfae5be46be7023a2a1dd459795a8d1944
Skkay <skkay.thomas@gmail.com>
2021-01-19 11:43:29 +0100

    Error message if internet connection is not available

--- app/src/main/java/fr/skkay/instabus/MainActivity.kt
+++ app/src/main/java/fr/skkay/instabus/MainActivity.kt
@@ -1,19 +1,45 @@
 package fr.skkay.instabus
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import fr.skkay.instabus.ui.main.SectionsPagerAdapter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        if (isNetworkConnected()) {
+            val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+            val viewPager: ViewPager = findViewById(R.id.view_pager)
+            viewPager.adapter = sectionsPagerAdapter
+            val tabs: TabLayout = findViewById(R.id.tabs)
+            tabs.setupWithViewPager(viewPager)
+        }
+        else {
+            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+            val textView = findViewById<TextView>(R.id.textview_network_issue)
+            textView.text = "Aucune connexion Internet"
+        }
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        var connected = false
+        try {
+            val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val nInfo = cm.activeNetworkInfo
+            connected = nInfo != null && nInfo.isAvailable && nInfo.isConnected
+            return connected
+        } catch (e: Exception) {
+            Log.e("Connectivity Exception", e.toString())
+        }
+        return connected
     }
 }
